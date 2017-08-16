0ff64df6a03ce898ebab601e35ed7bcf24f57786
gabemartinez <gibbi33@gmail.com>
2017-08-15 23:21:54 -0700

    Schema work.

--- models/Record.js
+++ models/Record.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 var recordSchema = new mongoose.Schema({
-  locationName: {type: String},
-  locationAddress: {type: String},
+  locationName: String,
+  locationAddress: String
 });
 
 module.exports = mongoose.model('Record', recordSchema);
