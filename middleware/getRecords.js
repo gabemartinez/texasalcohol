639f67faf90cccdb4e75f44bdcdb48494dc34f2e
Gabriel Martinez <gibbi33@gmail.com>
2017-08-18 00:01:59 -0700

    Data retrieval.

--- middleware/getRecords.js
+++ middleware/getRecords.js
@@ -1,38 +1,21 @@
 var getRecords = function(req, res, next) {
 
-  // var mongoose = require('mongoose');
-  // var mongoconnection = 'mongodb://txalcread:blakk865@ds011860.mlab.com:11860/txalcapp';
-  // mongoose.connect(mongoconnection);
-
   var mongoose = require("mongoose");
   mongoose.connect('mongodb://txalcread:blakk865@ds011860.mlab.com:11860/txalcapp');
   var connection = mongoose.connection;
 
   var permitnumber = req.params.permitnumber;
-  // console.log(permitnumber);
+  console.log(permitnumber);
 
-  connection.on('error', console.error.bind(console, 'connection error:'));
-  connection.once('open', function () {
+  var Record = require("../models/Record");
 
-      connection.db.collection("mixbevdata", function(err, collection){
-          collection.find({"tabcPermitNumber":permitnumber}).toArray(function(err, data){
-              console.log(data); // it will print your collection data
-          })
-      });
+  console.log(Record);
 
-  });
-
-  // var Record = require('../models/Record');
-
-  // var allrecordsobject = [];
-
-  // Record.find({"tabcPermitNumber": permitnumber}, function (err, records) {
+  // Record.find(function (err, records) {
   //     if (err) {
   //         res.status(500).send(err)
-  //         console.log('error');
   //     } else {
-  //         res.render('../views/pages/single-page-report', {reportid, pageid, buttonstests, unitnamegrade, globalasulinksgrade, buttonsgrade});
-  //         console.log(records);
+  //         res.send(records);
   //     }
   // });
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-var getrecords = require('../middleware/getRecords');
+// var getrecords = require('../middleware/getRecords');
 
 var mongoose = require('mongoose');
 var mongoconnection = 'mongodb://txalcread:blakk865@ds011860.mlab.com:11860/txalcapp';
 mongoose.connect(mongoconnection);
 
-var Record = require('../models/Record');
+var recordSchema = new mongoose.Schema({
+  locationName: String,
+  locationAddress: String
+});
+
+var Record = mongoose.model('Record', recordSchema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,9 +21,15 @@ router.get('/', function(req, res, next) {
 
 /* GET tabcPermitNumber documents page. */
 // ex. MB420026
-router.get('/permitnumber/:permitnumber', getrecords, function(req, res, next) {
+router.get('/permitnumber/:permitnumber', function(req, res, next) {
   var permitnumber = req.params.permitnumber;
-  var allrecordsobject = req.allrecordsobject;
+  // var allrecordsobject = req.allrecordsobject;
+
+  Record.find()
+  .then(function(doc){
+    console.log(doc);
+  })
+
   // res.render('permitnumber', { title: 'Documents by TABC Permit Number' });
   res.json( { title: 'Documents by TABC Permit Number', permitnumber });
 });
