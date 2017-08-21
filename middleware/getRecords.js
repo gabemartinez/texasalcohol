89d4c8ba9fc99fc4c74943f4dc27e55586c9c78a
Gabriel Martinez <gibbi33@gmail.com>
2017-08-20 23:26:32 -0700

    Data retrieval cont.

--- middleware/getRecords.js
+++ middleware/getRecords.js
@@ -1,25 +1,25 @@
 var getRecords = function(req, res, next) {
 
-  var mongoose = require("mongoose");
+  var mongoose = require('mongoose');
   mongoose.connect('mongodb://txalcread:blakk865@ds011860.mlab.com:11860/txalcapp');
   var connection = mongoose.connection;
 
   var permitnumber = req.params.permitnumber;
   console.log(permitnumber);
 
-  var Record = require("../models/Record");
+  var Record = require('../models/Record');
 
-  console.log(Record);
+  Record.find(function (err, records) {
+      if (err) {
+          res.status(500).send(err)
+      } else {
+          // res.send(records);
+          req.records = records;
+          next();
+      }
+  });
 
-  // Record.find(function (err, records) {
-  //     if (err) {
-  //         res.status(500).send(err)
-  //     } else {
-  //         res.send(records);
-  //     }
-  // });
-
-  next();
+  // next();
 
 };
 
--- models/Record.js
+++ models/Record.js
@@ -3,8 +3,32 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 var recordSchema = new mongoose.Schema({
+
+  taxpayerNumber: Number,
+  taxpayerName: String,
+  taxpayerAddress: String,
+  taxpayerCity: String,
+  taxpayerState: String,
+  taxpayerZip: Number,
+  taxpayerCounty: Number,
+  taxpayerPhone: Number,
+  locationNumber: Number,
   locationName: String,
-  locationAddress: String
+  locationAddress: String,
+  locationCity: String,
+  locationState: String,
+  locationZip: Number,
+  locationCounty: Number,
+  locationPhone: Number,
+  tabcPermitNumber: String,
+  responsibilityBeginDate: Number,
+  responsibilityEndDate: Number,
+  obligationEndDate: Number,
+  liquorReceipts: Number,
+  wineReceipts: Number,
+  beerReceipts: Number,
+  returnTotal: Number
+
 });
 
 module.exports = mongoose.model('Record', recordSchema);
